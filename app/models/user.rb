6b28807a2149fbc3ef9bea02a05a4c19094cd299
Nicolas Helfenberger <nhelfenberger@vev.fr>
2015-10-19 18:51:42 +0200

    models2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :flats, foreign_key: "owner_id"
-  has_many :bookings, foreign_key: "guest_id"
+  has_many :flats, foreign_key: "owner_id", depedent: :destroy
+  has_many :bookings, foreign_key: "guest_id",  depedent: :destroy
 
 end
