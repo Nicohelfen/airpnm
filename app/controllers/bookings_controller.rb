fc99d2151727edc78c1c56d1cb1f4c70b5654fed
Martin Debrunne <martin.debrunne@gmail.com>
2015-10-21 07:03:04 +0200

    Add to Bookings Controller :index (list bookings of flat to owner) :accept (owner can accept a booking from an user)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,16 +2,34 @@ class BookingsController < ApplicationController
   before_action :set_whitelist, only: [:create]
   before_action :authenticate_user!
 
+  def index
+    flat = Flat.find(params[:flat_id])
+    if flat && flat.owner == current_user
+      @bookings = flat.bookings
+    end
+  end
+
+  def accept
+    flat = Flat.find(params[:flat_id])
+    if current_user == flat.owner
+      booking = Booking.find(params[:booking_id])
+      booking.update(status: "OK")
+    end
+    redirect_to flat_bookings_path
+  end
+
   def new
-    @booking = Booking.new(guest_id: current_user, status: "waiting")
+    flat = Flat.find(params[:flat_id])
+    @booking = flat.bookings.build(guest: current_user, flat: flat)
   end
 
   def create
     flat = Flat.find(params[:flat_id])
-    booking = Booking.new(@whitelist, flat: flat)
+    @whitelist[:status] = "Waiting"
+    booking = Booking.new(@whitelist)
     booking.save
     if booking.valid?
-      redirect_to flat_path(@whitelist[:flat])
+      redirect_to flat_path(flat)
     else
       render "new"
     end
@@ -19,6 +37,6 @@ class BookingsController < ApplicationController
 
   private
   def set_whitelist
-    @whitelist = params.require(:booking).permit(:check_in, :check_out, :guest_id)
+    @whitelist = params.require(:booking).permit(:check_in, :check_out, :guest_id, :flat_id)
   end
 end
