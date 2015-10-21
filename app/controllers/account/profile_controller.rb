1115bf2ee24386746fcbb1dc47c5d9646aec7383
Martin Debrunne <martin.debrunne@gmail.com>
2015-10-21 16:35:15 +0200

    new routes for profile, CTRL and VIEWS

---
+++ app/controllers/account/profile_controller.rb
@@ -0,0 +1,6 @@
+module Account
+  class ProfileController < ApplicationController
+    def edit
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  devise_for :users
+  devise_for :users, expect: [:sessions]
 
   resources :flats, only: [:show ] do
     resources :bookings, only: [:new, :create]
   end
+
   namespace :account do
+    root to: "profile#edit"
     resources :flats, only: [:index, :new, :create, :edit, :update]
+    resource :profile, only: [:edit], controller: "profile"
     resources :bookings, only: [:index]  do
       member do
         put :accept
