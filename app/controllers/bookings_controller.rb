3be2311f71e145275788c642a6d371383b8447bf
Martin Debrunne <martin.debrunne@gmail.com>
2015-10-21 09:07:49 +0200

    fiex dry on Bookings Controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,16 @@
 class BookingsController < ApplicationController
   before_action :set_whitelist, only: [:create]
   before_action :authenticate_user!
+  before_action :set_flat, only: [:index, :accept, :new, :create]
 
   def index
-    flat = Flat.find(params[:flat_id])
-    if flat && flat.owner == current_user
-      @bookings = flat.bookings
+    if @flat && @flat.owner == current_user
+      @bookings = @flat.bookings
     end
   end
 
   def accept
-    flat = Flat.find(params[:flat_id])
-    if current_user == flat.owner
+    if current_user == @flat.owner
       booking = Booking.find(params[:booking_id])
       booking.update(status: "OK")
     end
@@ -19,23 +18,25 @@ class BookingsController < ApplicationController
   end
 
   def new
-    flat = Flat.find(params[:flat_id])
-    @booking = flat.bookings.build(guest: current_user, flat: flat)
+    @booking = @flat.bookings.build(guest: current_user, flat: @flat)
   end
 
   def create
-    flat = Flat.find(params[:flat_id])
     @whitelist[:status] = "Waiting"
     booking = Booking.new(@whitelist)
     booking.save
     if booking.valid?
-      redirect_to flat_path(flat)
+      redirect_to flat_path(@flat)
     else
       render "new"
     end
   end
 
   private
+  def set_flat
+    @flat = Flat.find(params[:flat_id])
+  end
+
   def set_whitelist
     @whitelist = params.require(:booking).permit(:check_in, :check_out, :guest_id, :flat_id)
   end
