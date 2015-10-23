a9a028ac669cf0c71eb43f22a4c8fcd1966bc1e2
Pierre Le Govic <pierrelg@me.com>
2015-10-23 16:05:24 +0200

    last changes to profile and homepage

--- app/controllers/account/profile_controller.rb
+++ app/controllers/account/profile_controller.rb
@@ -1,9 +1,27 @@
 module Account
   class ProfileController < ApplicationController
-    def
+
+    def show
     end
 
     def edit
+      @profile = current_user
+    end
+
+    def update
+      @profile = current_user
+      if @profile.update(params_whitelist)
+        flash[:notice] = "Your profile has been updated"
+        redirect_to account_profile_path
+      else
+        flash[:alert] = "Your profile has not been updated? Please try again"
+        render :edit
+      end
+    end
+
+    private
+    def params_whitelist
+      params.require(:user).permit(:first_name, :last_name, :email, :picture)
     end
   end
 end
